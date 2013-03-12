ec378ac0e3a70185cff142cb36d34feae7bf0ec4
Nathan Levine <Nathan@Nathans-MacBook-Pro.local>
2013-03-12 19:25:28 -0400

    Nathan's Annotation changes

--- TakeMeNearThere/TMNTViewController.h
+++ TakeMeNearThere/TMNTViewController.h
@@ -9,6 +9,38 @@
 #import <UIKit/UIKit.h>
 #import "TMNTDataSourceDelegate.h"
 
+
 @interface TMNTViewController : UIViewController <TMNTDataSourceDelegate>
 
+@property (strong, nonatomic) NSMutableArray *returnedArray;
+
+-(void)addPinsToMap;
+
 @end
+
+
+
+/* //things i(Nathan) did
+ 
+ 1. make new annotation class
+ 2. in annotation class import mapkit framework
+ 3. make annotation class an <mkannotation> delegate
+ 4. add title, subtitle, and coordinate properties and synthesize dem badboys
+ 
+ 5. import annotation.h to vc.m
+ 6. make new method in vc (addPinsToMap)
+ 7. import coreloaction framework to vc.m
+ 8. gave returnedArray class scope instead of just method local scope
+ 9. added custom init to annotation class
+ 10. added stuff to addpins method
+ 
+ 11.CANT GET IT TO CALL WTF --NVM GOT IT!!:)
+ 
+ 12. added users location (checkbox :))
+ 13.make region normal so you dont have to zoom like crazy
+ 14. made tmnt location  *location an instance variable
+
+ 
+ 
+ 
+*/
